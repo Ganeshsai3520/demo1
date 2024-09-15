1d341d4592e0fee18e870b3ebafde76bb2de757a
ganesh <ganeshm4087@gmail.com>
2024-09-15 22:43:34 +0530

    indian commit

--- src/test/java/graphql/git_prac.java
+++ src/test/java/graphql/git_prac.java
@@ -2,6 +2,10 @@ package graphql;
 
 public class git_prac {
 	public static void main(String[] args) {
-	System.out.println("changes Done");
+	System.out.println("1 changes Done");
+	System.out.println("2 changes Done");
+	System.out.println("3 changes Done");
+	System.out.println("4 changes Done");
+	System.out.println("5 changes Done");
 }
 }
